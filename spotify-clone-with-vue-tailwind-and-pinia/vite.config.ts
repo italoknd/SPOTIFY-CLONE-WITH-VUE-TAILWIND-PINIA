f87aa7a4b46f58e840d1ac09f369f9acf6b579a4
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-01-28 19:28:13 -0400

    installing reactivity transform macro

--- spotify-clone-with-vue-tailwind-and-pinia/vite.config.ts
+++ spotify-clone-with-vue-tailwind-and-pinia/vite.config.ts
@@ -1,7 +1,8 @@
+import ReactivityTransform from '@vue-macros/reactivity-transform/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), ReactivityTransform()],
 })
