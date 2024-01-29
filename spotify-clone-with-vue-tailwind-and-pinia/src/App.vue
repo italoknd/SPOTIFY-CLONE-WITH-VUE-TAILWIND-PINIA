45c5b9c459c359e532d84de1cad21925a570dd23
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-01-29 17:57:30 -0400

    adding favicon, tailwind, vue router, pinia and vue material icons

--- spotify-clone-with-vue-tailwind-and-pinia/src/App.vue
+++ spotify-clone-with-vue-tailwind-and-pinia/src/App.vue
@@ -1,10 +1,25 @@
 <script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+
+//ICONS
+import ChevronUp from "vue-material-design-icons/ChevronUp.vue";
+import ChevronDown from "vue-material-design-icons/ChevronDown.vue";
+import ChevronRight from "vue-material-design-icons/ChevronRight.vue";
+import ChevronLeft from "vue-material-design-icons/ChevronLeft.vue";
+
+//HOOKS
+
+//VARIABLES
 </script>
 
 <template>
-  <div>
+  <div
+    class="w-[calc(100%-20px)] h-[60px] fixed right-0 z-20 bg-[#101010] bg-opacity-80 flex items-center justify-between"
+  >
+    <div >
+
+    </div>
   </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
