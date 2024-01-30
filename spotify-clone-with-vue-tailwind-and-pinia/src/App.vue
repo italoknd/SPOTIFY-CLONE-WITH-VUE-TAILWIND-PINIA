7a7fea4380cea99a4934c1aa0f1d16c18d952ad4
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-01-30 00:14:57 -0400

    adding images from the original app

--- spotify-clone-with-vue-tailwind-and-pinia/src/App.vue
+++ spotify-clone-with-vue-tailwind-and-pinia/src/App.vue
@@ -26,7 +26,7 @@ const toggleOpeness = (param: boolean): void => {
 <template>
   <div>
     <div
-      class="w-[calc(100%-20px)] h-[60px] fixed right-0 z-20 bg-[#101010] bg-opacity-80 flex items-center justify-between"
+      class="w-[calc(100%-230px)] h-[60px] fixed right-0 z-20 bg-[#101010] bg-opacity-80 flex items-center justify-between"
     >
       <div class="flex items-center ml-6">
         <button
@@ -42,10 +42,12 @@ const toggleOpeness = (param: boolean): void => {
           <ChevronRight fillColor="#fff" :size="30" />
         </button>
       </div>
+
+      <!--PROFILE INFOS-->
       <button
         @click="toggleOpeness(!openMenu)"
-        :class="openMenu ? 'bg-[#282828]' : 'bg-black'"
-        class="bg-black hover:bg-[#282828] rounded-full p-0.5 mr-8 mt-0.5 cursor-pointer"
+        :class="openMenu ? 'bg-secondary' : 'bg-black'"
+        class="bg-black hover:bg-secondary rounded-full p-0.5 mr-8 mt-0.5 cursor-pointer"
       >
         <div class="flex items-center">
           <img
@@ -57,21 +59,27 @@ const toggleOpeness = (param: boolean): void => {
           <div class="text-white text-[14px] mx-1.5 font-semibold">
             {{ username }}
           </div>
-          <ChevronDown
-            v-if="openMenu"
-            @click="toggleOpeness(true)"
-            fillColor="#fff"
-            :size="25"
-          />
-          <ChevronUp
-            v-else
-            @click="toggleOpeness(false)"
-            fillColor="#fff"
-            :size="25"
-          />
+          <ChevronDown v-if="openMenu" fillColor="#fff" :size="25" />
+          <ChevronUp v-else fillColor="#fff" :size="25" />
         </div>
       </button>
+      <span
+        v-if="openMenu"
+        class="fixed w-[190px] bg-[#282828] shadow-2xl z-50 rounded-sm top-[52px] right-[35px] p-1 cursor-pointer"
+      >
+        <ul class="text-gray-200 font-semibold text-[14px]">
+          <li class="px-3 py-2.5 hover:bg-[#3e3d3d] border-b-gray-600">
+            Profile
+          </li>
+          <li class="px-3 py-2.5 hover:bg-[#3e3d3d] border-b-gray-600">
+            Log out
+          </li>
+        </ul>
+      </span>
     </div>
+
+    <!--SIDEBAR-->
+    <div class="h-[100%] p-6 w-[240px] fixed z-50 bg-black" id="sideNav"></div>
   </div>
 </template>
 
