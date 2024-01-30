32723dc3b55ddd96fde9cd3f61f37d7890043272
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-01-30 01:01:26 -0400

    creating new component for menu items

---
+++ spotify-clone-with-vue-tailwind-and-pinia/src/components/MenuItem.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+import { IMenuItems } from "../interfaces/menuItemsProps";
+
+const route = useRoute();
+const props = defineProps<IMenuItems>();
+</script>
+<template>
+  <div></div>
+</template>
