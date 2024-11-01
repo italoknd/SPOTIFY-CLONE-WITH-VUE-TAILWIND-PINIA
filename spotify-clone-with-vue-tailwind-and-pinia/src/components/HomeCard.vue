bc59c8f5d49c9023aae2b26839df1c7680fc1383
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-11-01 17:43:36 -0400

    feat: build card component for the home

---
+++ spotify-clone-with-vue-tailwind-and-pinia/src/components/HomeCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <RouterLink to="library">
+      <div
+        class="bg-[#111111] p-4 rounded m-2 hover:bg-[#252525] cursor-pointer"
+      >
+        <img :src="image" class="rounded-md" />
+        <div class="text-white pt-4 font-semibold text-[17px]">
+          {{ title }}
+        </div>
+        <div class="text-white pt-1 pb-3 text-[14px]">
+          {{ subtitle }}
+        </div>
+      </div>
+    </RouterLink>
+  </div>
+</template>
+<script setup lang="ts">
+import { IHomeCard } from "../interfaces/homeCard";
+import { RouterLink } from "vue-router";
+const props = defineProps<IHomeCard>();
+</script>
