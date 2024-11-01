ba5e097297cb69557a315bb8ee1bf21d4eabaa7a
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-11-01 17:40:39 -0400

    feat: adding podcast cards

---
+++ spotify-clone-with-vue-tailwind-and-pinia/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="p-8">
+    <button
+      type="button"
+      class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+    >
+      Podcasts to make you smarter
+    </button>
+
+    <div class="py-1.5"></div>
+    <div class="flex items-centers">
+      <HomeCard
+        v-for="(card, index) in cards"
+        :key="index"
+        :image="card.image"
+        :title="card.title"
+        :subtitle="card.subtitle"
+      />
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { cards } from "../scripts/homeCard";
+import HomeCard from "../components/HomeCard.vue";
+</script>
