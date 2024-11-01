fc3a5f28ccc6f1cf1b83d9adbac9eebe33e9fe00
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-11-01 19:09:52 -0400

    changing some tracks

--- spotify-clone-with-vue-tailwind-and-pinia/src/scripts/homeCard.ts
+++ spotify-clone-with-vue-tailwind-and-pinia/src/scripts/homeCard.ts
@@ -34,53 +34,55 @@ export const podcast_cards: IHomeCard[] = [
 ];
 export const recommended_radio_cards: IHomeCard[] = [
   {
-    image: "https://picsum.photos/id/30/300/300",
-    title: "Morning Talk",
-    subtitle: "Hawk Group",
+    image: "https://picsum.photos/id/11/300/300",
+    title: "Nature Radio",
+    subtitle: "Find your vibes here",
     styles: "",
   },
   {
-    image: "https://picsum.photos/id/45/300/300",
-    title: "Deli Podcast",
-    subtitle: "Dharma Group",
+    image: "https://picsum.photos/id/53/300/300",
+    title: "Blue Radio",
+    subtitle: "Songs that takes you into another level.",
     styles: "",
   },
   {
-    image: "https://picsum.photos/id/65/300/300",
-    title: "Girls Podcast",
-    subtitle: "Girls Podcast",
+    image: "https://picsum.photos/id/78/300/300",
+    title: "Antique Radio",
+    subtitle: "Songs of antique.",
     styles: "md:block hidden",
   },
   {
-    image: "https://picsum.photos/id/67/300/300",
-    title: "CampCast",
-    subtitle: "CampCast",
+    image: "https://picsum.photos/id/84/300/300",
+    title: "Seatlle Radio",
+    subtitle: "Radio of Seatlle.",
     styles: "lg:block hidden",
   },
   {
-    image: "https://picsum.photos/id/100/300/300",
-    title: "The World Before Us",
-    subtitle: "Antique Podcast",
+    image: "https://picsum.photos/id/96/300/300",
+    title: "Radio Gamer",
+    subtitle: "Radio for listen while gaming.",
     styles: "xl:block hidden",
   },
 ];
 export const recommended_songs_cards: IHomeCard[] = [
   {
-    image: "https://picsum.photos/id/30/300/300",
-    title: "Morning Talk",
-    subtitle: "Hawk Group",
+    image:
+      "https://i.ytimg.com/vi/emYmcAUSEEM/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAzzdRyBbvCJnTneag28fZhIN3azg",
+    title: "Martin Garrix",
+    subtitle: "Discover the latest track from Martin Garrix.",
     styles: "",
   },
   {
-    image: "https://picsum.photos/id/45/300/300",
-    title: "Deli Podcast",
-    subtitle: "Dharma Group",
+    image:
+      "https://i.scdn.co/image/ab67616d0000b2732cd834c826d484dcb3414699",
+    title: "Martin Garrix & Third Party",
+    subtitle: "Discover the latest track from Martin Garrix and Third Party.",
     styles: "",
   },
   {
-    image: "https://picsum.photos/id/65/300/300",
-    title: "Girls Podcast",
-    subtitle: "Girls Podcast",
+    image: "https://i.scdn.co/image/ab67616d0000b273aca64d6105d38b07b061b1f7",
+    title: "MORTEN",
+    subtitle: "Discover the latest track from MORTEN.",
     styles: "md:block hidden",
   },
   {
