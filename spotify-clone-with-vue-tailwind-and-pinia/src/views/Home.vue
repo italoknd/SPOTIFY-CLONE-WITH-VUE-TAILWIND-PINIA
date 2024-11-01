381c37900bd86c6dd1ebcf5518fc8b4b01d3a771
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-11-01 18:17:16 -0400

    refactor: refactor home page to automate the adding process of a new section

--- spotify-clone-with-vue-tailwind-and-pinia/src/views/Home.vue
+++ spotify-clone-with-vue-tailwind-and-pinia/src/views/Home.vue
@@ -1,25 +1,28 @@
 <template>
-  <div class="p-8">
-    <button
-      type="button"
-      class="text-white text-2xl font-semibold hover:underline cursor-pointer"
-    >
-      Podcasts to make you smarter
-    </button>
+  <div v-for="(section, index) in sections" :key="index">
+    <div class="p-8">
+      <button
+        type="button"
+        class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+      >
+        {{ section.section_name }}
+      </button>
 
-    <div class="py-1.5"></div>
-    <div class="flex items-centers">
-      <HomeCard
-        v-for="(card, index) in cards"
-        :key="index"
-        :image="card.image"
-        :title="card.title"
-        :subtitle="card.subtitle"
-      />
+      <div class="py-1.5"></div>
+      <div class="flex items-centers overflow-hidden">
+        <HomeCard
+          v-for="(card, index) in section.card"
+          :key="index"
+          :image="card.image"
+          :title="card.title"
+          :subtitle="card.subtitle"
+          :class="card.styles"
+        />
+      </div>
     </div>
   </div>
 </template>
 <script setup lang="ts">
-import { cards } from "../scripts/homeCard";
+import { sections } from "../scripts/sections";
 import HomeCard from "../components/HomeCard.vue";
 </script>
