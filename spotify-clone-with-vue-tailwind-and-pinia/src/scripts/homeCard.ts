68c72c3b2d1dfb6c21034457e790fa1de71277e8
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-11-01 18:16:39 -0400

    feat: adding new sections

--- spotify-clone-with-vue-tailwind-and-pinia/src/scripts/homeCard.ts
+++ spotify-clone-with-vue-tailwind-and-pinia/src/scripts/homeCard.ts
@@ -1,29 +1,98 @@
 import { IHomeCard } from "../interfaces/homeCard";
 
-export const cards: IHomeCard[] = [
+export const podcast_cards: IHomeCard[] = [
   {
     image: "https://picsum.photos/id/30/300/300",
     title: "Morning Talk",
     subtitle: "Hawk Group",
+    styles: "",
   },
   {
     image: "https://picsum.photos/id/45/300/300",
     title: "Deli Podcast",
     subtitle: "Dharma Group",
+    styles: "",
   },
   {
     image: "https://picsum.photos/id/65/300/300",
     title: "Girls Podcast",
     subtitle: "Girls Podcast",
+    styles: "md:block hidden",
   },
   {
     image: "https://picsum.photos/id/67/300/300",
     title: "CampCast",
     subtitle: "CampCast",
+    styles: "lg:block hidden",
   },
   {
     image: "https://picsum.photos/id/100/300/300",
     title: "The World Before Us",
     subtitle: "Antique Podcast",
+    styles: "xl:block hidden",
+  },
+];
+export const recommended_radio_cards: IHomeCard[] = [
+  {
+    image: "https://picsum.photos/id/30/300/300",
+    title: "Morning Talk",
+    subtitle: "Hawk Group",
+    styles: "",
+  },
+  {
+    image: "https://picsum.photos/id/45/300/300",
+    title: "Deli Podcast",
+    subtitle: "Dharma Group",
+    styles: "",
+  },
+  {
+    image: "https://picsum.photos/id/65/300/300",
+    title: "Girls Podcast",
+    subtitle: "Girls Podcast",
+    styles: "md:block hidden",
+  },
+  {
+    image: "https://picsum.photos/id/67/300/300",
+    title: "CampCast",
+    subtitle: "CampCast",
+    styles: "lg:block hidden",
+  },
+  {
+    image: "https://picsum.photos/id/100/300/300",
+    title: "The World Before Us",
+    subtitle: "Antique Podcast",
+    styles: "xl:block hidden",
+  },
+];
+export const recommended_songs_cards: IHomeCard[] = [
+  {
+    image: "https://picsum.photos/id/30/300/300",
+    title: "Morning Talk",
+    subtitle: "Hawk Group",
+    styles: "",
+  },
+  {
+    image: "https://picsum.photos/id/45/300/300",
+    title: "Deli Podcast",
+    subtitle: "Dharma Group",
+    styles: "",
+  },
+  {
+    image: "https://picsum.photos/id/65/300/300",
+    title: "Girls Podcast",
+    subtitle: "Girls Podcast",
+    styles: "md:block hidden",
+  },
+  {
+    image: "https://picsum.photos/id/67/300/300",
+    title: "CampCast",
+    subtitle: "CampCast",
+    styles: "lg:block hidden",
+  },
+  {
+    image: "https://picsum.photos/id/100/300/300",
+    title: "The World Before Us",
+    subtitle: "Antique Podcast",
+    styles: "xl:block hidden",
   },
 ];
