86edfbe7d1ffd3934da36cc3fbc51753c8d95494
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-11-01 19:09:41 -0400

    adding some css

--- spotify-clone-with-vue-tailwind-and-pinia/src/components/HomeCard.vue
+++ spotify-clone-with-vue-tailwind-and-pinia/src/components/HomeCard.vue
@@ -2,13 +2,18 @@
   <div>
     <RouterLink to="library">
       <div
-        class="bg-[#111111] p-4 rounded m-2 hover:bg-[#252525] cursor-pointer"
+        class="bg-[#111111] p-4 rounded m-2 hover:bg-[#252525] cursor-pointer "
       >
-        <img :src="image" class="rounded-md" />
-        <div class="text-white pt-4 font-semibold text-[17px]">
+        <img
+          :src="image"
+          class="rounded-md sm:w-[300px] sm:h-[150px] lg:w-[300px] lg:h-[250px] xl:w-[300px] xl:h-[300px] object-cover"
+        />
+        <div class="text-white pt-4 font-semibold text-[17px] overflow-hidden text-ellipsis">
           {{ title }}
         </div>
-        <div class="text-white pt-1 pb-3 text-[14px]">
+        <div
+          class="text-white pt-1 pb-3 text-[14px] xl:h-[45px] lg:h-[35px] md:h-[25px] sm:h-[15px] overflow-hidden text-ellipsis"
+        >
           {{ subtitle }}
         </div>
       </div>
