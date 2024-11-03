c093917473348d682d84c1c8a41cbeefe7b79499
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-11-03 00:13:59 -0400

    feat: add a new section of search

---
+++ spotify-clone-with-vue-tailwind-and-pinia/src/views/Search.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="p-8">
+    <button
+      type="button"
+      class="text-white text-2xl pl-3 font-semibold hover:underline cursor-pointer"
+    >
+      Seek new vibes for you!
+    </button>
+
+    <div class="py-1.5"></div>
+    <Category />
+  </div>
+</template>
+<script setup lang="ts">
+import Category from "../components/Category.vue";
+</script>
