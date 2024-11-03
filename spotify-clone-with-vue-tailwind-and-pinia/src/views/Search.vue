9eecbd02607ef6970e53dff90064cef89837f858
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-11-03 00:34:14 -0400

    feat: loop over the list of categories on search view

--- spotify-clone-with-vue-tailwind-and-pinia/src/views/Search.vue
+++ spotify-clone-with-vue-tailwind-and-pinia/src/views/Search.vue
@@ -8,9 +8,16 @@
     </button>
 
     <div class="py-1.5"></div>
-    <Category />
+    <div
+      class="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-6"
+    >
+      <div v-for="(category, index) in categories" :key="index">
+        <Category :category="category.category" :image="category.image" />
+      </div>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
+import { categories } from "../scripts/categories";
 import Category from "../components/Category.vue";
 </script>
