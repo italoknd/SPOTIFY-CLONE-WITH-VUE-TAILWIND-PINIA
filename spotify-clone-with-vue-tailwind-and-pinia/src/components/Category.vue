3e209c9b80923dd58d517813e5c16cf78c3c1a91
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-11-03 00:34:48 -0400

    fix: fix random color generating error by removing the loop on the category component

--- spotify-clone-with-vue-tailwind-and-pinia/src/components/Category.vue
+++ spotify-clone-with-vue-tailwind-and-pinia/src/components/Category.vue
@@ -1,29 +1,30 @@
 <template>
-  <div class="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-6">
-    <RouterLink to="library" v-for="(category, index) in categories" :key="index">
+  <div>
+    <RouterLink to="library">
       <div
         v-if="randColor"
-        :style="{ backgroundColor: randColor.color }"
         class="contrast-[2] brightness-[0.87] rounded-xl aspect-square relative overflow-hidden"
+        :style="{ backgroundColor: randColor.color }"
       >
         <div class="text-white pt-3 pl-4 font-semibold text-[23px]">
-          {{ category.category }}
+          {{ category }}
         </div>
         <img
-          :src="category.image"
-          class="absolute contrast-[0.55] -right-3.5 bottom-0 max-w-[100px] rotate-[20deg]"
+          :src="image"
+          class="absolute contrast-[0.55] -right-3.5 bottom-0 max-w-[175px] rotate-[20deg]"
         />
       </div>
     </RouterLink>
   </div>
 </template>
 <script setup lang="ts">
-import { categories } from "../scripts/categories";
 import { IUniqueColor } from "../interfaces/uniqolor";
 import { RouterLink } from "vue-router";
 import { ref } from "vue";
 import uniqolor from "uniqolor";
+import { ICategory } from "../interfaces/category";
 
 let randColor = ref<IUniqueColor>({ color: "", isLight: false });
 randColor.value = uniqolor.random();
+const props = defineProps<ICategory>();
 </script>
