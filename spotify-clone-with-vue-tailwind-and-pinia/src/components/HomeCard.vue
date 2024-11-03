dd221c7af7b4206ced762c97b8099c1e97143725
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-11-02 20:12:36 -0400

    feat: add some css for other screen sizes

--- spotify-clone-with-vue-tailwind-and-pinia/src/components/HomeCard.vue
+++ spotify-clone-with-vue-tailwind-and-pinia/src/components/HomeCard.vue
@@ -2,17 +2,19 @@
   <div>
     <RouterLink to="library">
       <div
-        class="bg-[#111111] p-4 rounded m-2 hover:bg-[#252525] cursor-pointer "
+        class="bg-[#111111] p-4 rounded m-2 hover:bg-[#252525] cursor-pointer"
       >
         <img
           :src="image"
-          class="rounded-md sm:w-[300px] sm:h-[150px] lg:w-[300px] lg:h-[250px] xl:w-[300px] xl:h-[300px] object-cover"
+          class="rounded-md w-[300px] h-[150px] sm:w-[300px] sm:h-[150px] lg:w-[300px] lg:h-[250px] xl:w-[300px] xl:h-[300px] object-cover"
         />
-        <div class="text-white pt-4 font-semibold text-[17px] overflow-hidden text-ellipsis">
+        <div
+          class="text-white pt-2 xl:pt-4 lg:pt-3 md:pt-0.5 sm:p-[6px] font-semibold text-[17px] w-[100px] h-[60px] xl:h-[40px] lg:h-[40px] md:h-[25px] sm:h-[25px] xl:w-[270px] sm:w-[170px] overflow-hidden text-ellipsis"
+        >
           {{ title }}
         </div>
         <div
-          class="text-white pt-1 pb-3 text-[14px] xl:h-[45px] lg:h-[35px] md:h-[25px] sm:h-[15px] overflow-hidden text-ellipsis"
+          class="text-white pt-1 pb-3 text-[14px] w-[100px] max-h-[50px] xl:h-[45px] lg:h-[35px] md:h-[25px] sm:h-[15px] xl:w-[270px] lg:w-[180px] md:w-[150px] sm:w-[100px] overflow-hidden text-ellipsis"
         >
           {{ subtitle }}
         </div>
@@ -24,4 +26,6 @@
 import { IHomeCard } from "../interfaces/homeCard";
 import { RouterLink } from "vue-router";
 const props = defineProps<IHomeCard>();
+const debug_css =
+  "xl:bg-green-500 lg:bg-blue-500 md:bg-yellow-400 sm:bg-red-500";
 </script>
