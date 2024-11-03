45db8d3af9ce01baee12708d4426355f671ab8c9
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-11-03 14:06:11 -0400

    feat: creating the lib component

---
+++ spotify-clone-with-vue-tailwind-and-pinia/src/views/Library.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="p-8 rounded-md grid grid-cols-4 gap-6">
+    <div v-for="(album, index) in albums" :key="index">
+      <div class="flex bg-gray-600 rounded-md duration-300 hover:cursor-pointer hover:bg-gray-300">
+        <img :src="album.album_cover" class="w-[60px] h-[60px] rounded-s-md" />
+        <div class="ml-3 mr-5 flex items-center justify-between w-full">
+            <p class="font-semibold">{{album.album}}</p>
+            <div class="circle">
+                <Play :size="25"/>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+import all_albums from "../../artist.json";
+import { IAlbum } from "../interfaces/albums";
+
+import Play from "vue-material-design-icons/Play.vue";
+import Pause from "vue-material-design-icons/Pause.vue";
+import DotsHorizontal from "vue-material-design-icons/DotsHorizontal.vue";
+import Heart from "vue-material-design-icons/Heart.vue";
+import ClockTimeThreeOutline from "vue-material-design-icons/ClockTimeThreeOutline.vue";
+
+const albums = ref<IAlbum[]>(all_albums);
+</script>
