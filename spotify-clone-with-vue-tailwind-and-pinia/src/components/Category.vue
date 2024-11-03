4efa9feb62cf98b5731adcd9f7127635eaed0308
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-11-03 03:30:12 -0400

    feat: adding some css

--- spotify-clone-with-vue-tailwind-and-pinia/src/components/Category.vue
+++ spotify-clone-with-vue-tailwind-and-pinia/src/components/Category.vue
@@ -6,12 +6,12 @@
         class="contrast-[2] brightness-[0.87] rounded-xl aspect-square relative overflow-hidden hover:opacity-80"
         :style="{ backgroundColor: randColor.color }"
       >
-        <div class="text-white pt-3 pl-4 font-semibold text-[23px]">
+        <div class="text-white pt-3 pl-4 font-semibold text-[23px] sm:pt-0.5 overflow-hidden text-ellipsis">
           {{ category }}
         </div>
         <img
           :src="image"
-          class="absolute contrast-[0.55] -right-3.5 bottom-0 max-w-[175px] rotate-[20deg] duration-300 hover:rotate-[0deg] hover:scale-150 hover:rounded-md"
+          class="absolute contrast-[0.55] -right-3.5 bottom-0 rotate-[20deg] duration-300 hover:rotate-[0deg] hover:scale-150 hover:rounded-md 2xl:max-w-[175px] 2xl:min-w-[155px] 2xl:hover:scale-110 xl:w-[115px] lg:w-[120px] lg:hover:scale-100 md:max-w-[110px] md:min-w-[50px] md:hover:scale-120 sm:max-w-[125px] "
         />
       </div>
     </RouterLink>
