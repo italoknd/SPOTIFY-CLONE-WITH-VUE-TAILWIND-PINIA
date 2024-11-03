d5888dff0c831c7f0417b90fb807798c10b09d7c
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-11-03 00:14:16 -0400

    feat: create a new list for the search section

---
+++ spotify-clone-with-vue-tailwind-and-pinia/src/scripts/categories.ts
@@ -0,0 +1,16 @@
+import { ICategory } from "../interfaces/category";
+
+export const categories: ICategory[] = [
+  {
+    category: "Podcasts",
+    image: "https://picsum.photos/id/238/300/300",
+  },
+  {
+    category: "Radios",
+    image: "https://picsum.photos/id/157/300/300",
+  },
+  {
+    category: "Spinnin' Records",
+    image: "https://picsum.photos/id/299/300/300",
+  },
+];
