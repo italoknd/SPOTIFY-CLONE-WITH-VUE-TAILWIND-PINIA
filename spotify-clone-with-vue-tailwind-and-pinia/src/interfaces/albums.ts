6e241bf41dca01070016e9bcace17eab0d8299f4
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-11-03 16:00:32 -0400

    feat: add new props on the interface

--- spotify-clone-with-vue-tailwind-and-pinia/src/interfaces/albums.ts
+++ spotify-clone-with-vue-tailwind-and-pinia/src/interfaces/albums.ts
@@ -1,9 +1,12 @@
 export interface IAlbum {
-  artist_of_the_album: string;
-  album: string;
-  album_category: string;
-  album_cover: string;
-  release_year: string;
+  artist_of_the_album?: string;
+  album?: string;
+  album_category?: string;
+  album_cover?: string;
+  release_year?: string;
+  public_playlist?: boolean;
+  playlist_name?: string;
+  playlist_cover?: string;
   tracks: ITrack[];
 }
 
