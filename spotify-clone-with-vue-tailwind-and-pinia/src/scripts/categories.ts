e81d2274cb8487558f58856c1759ede52a869016
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-11-03 00:33:41 -0400

    feat: populate categories list

--- spotify-clone-with-vue-tailwind-and-pinia/src/scripts/categories.ts
+++ spotify-clone-with-vue-tailwind-and-pinia/src/scripts/categories.ts
@@ -13,4 +13,52 @@ export const categories: ICategory[] = [
     category: "Spinnin' Records",
     image: "https://picsum.photos/id/299/300/300",
   },
+  {
+    category: "Podcasts",
+    image: "https://picsum.photos/id/238/300/300",
+  },
+  {
+    category: "Radios",
+    image: "https://picsum.photos/id/157/300/300",
+  },
+  {
+    category: "Spinnin' Records",
+    image: "https://picsum.photos/id/299/300/300",
+  },
+  {
+    category: "Podcasts",
+    image: "https://picsum.photos/id/238/300/300",
+  },
+  {
+    category: "Audiobooks",
+    image: "https://picsum.photos/id/157/300/300",
+  },
+  {
+    category: "Made For You",
+    image: "https://picsum.photos/id/299/300/300",
+  },
+  {
+    category: "New Release",
+    image: "https://picsum.photos/id/238/300/300",
+  },
+  {
+    category: "New Release",
+    image: "https://picsum.photos/id/157/300/300",
+  },
+  {
+    category: "New Release",
+    image: "https://picsum.photos/id/299/300/300",
+  },
+  {
+    category: "Podcasts",
+    image: "https://picsum.photos/id/238/300/300",
+  },
+  {
+    category: "Radios",
+    image: "https://picsum.photos/id/157/300/300",
+  },
+  {
+    category: "Spinnin' Records",
+    image: "https://picsum.photos/id/299/300/300",
+  },
 ];
