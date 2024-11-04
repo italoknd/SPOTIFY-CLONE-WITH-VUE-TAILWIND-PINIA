93c190a064e52e2c471f68a4c3fb2db381d194b3
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-11-03 21:52:20 -0400

    feat: add a new path for album details

--- spotify-clone-with-vue-tailwind-and-pinia/src/router/index.ts
+++ spotify-clone-with-vue-tailwind-and-pinia/src/router/index.ts
@@ -3,12 +3,19 @@ import { createRouter, createWebHashHistory } from "vue-router"; //importando m√
 import Home from "../views/Home.vue";
 import Search from "../views/Search.vue";
 import Library from "../views/Library.vue";
+import AlbumDetails from "../views/AlbumDetails.vue";
 
 const routes = [
-  // { path: "/", component: App },
   { path: "/", component: Home },
   { path: "/search", component: Search },
-  { path: "/library", component: Library },
+  {
+    path: "/library",
+    component: Library,
+  },
+  {
+    path: "/selected-album",
+    component: AlbumDetails,
+  },
 ];
 
 const router = createRouter({
