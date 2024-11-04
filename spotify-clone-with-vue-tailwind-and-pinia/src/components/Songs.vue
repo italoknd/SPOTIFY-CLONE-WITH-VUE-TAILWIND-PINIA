63cfa730fd7ad7c32eef393df94e4c7ae948c7b9
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-11-04 13:04:50 -0400

    feat: creating new component for the song list

---
+++ spotify-clone-with-vue-tailwind-and-pinia/src/components/Songs.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex -mt-[100px] glass-effect">
+      <div>
+        <div class="circle">
+          <Play :size="40" fillColor="#fff" />
+        </div>
+      </div>
+      <Heart class="mt-1 ml-4" :size="40" fillColor="#fff" />
+    </div>
+  </template>
+  <script setup lang="ts">
+  import { ref } from "vue";
+  import { useSelectAlbumStore } from "../store/selectAlbum.ts";
+  import { IAlbum } from "../interfaces/albums";
+  
+  const albumStore = useSelectAlbumStore();
+  const album_details = ref<IAlbum>(albumStore.album);
+  </script>
