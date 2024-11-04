8e3c5e4a0c50645302484acfa7486c025519a570
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-11-03 21:53:16 -0400

    feat: add router to redirect user for the album detail section

--- spotify-clone-with-vue-tailwind-and-pinia/src/views/Library.vue
+++ spotify-clone-with-vue-tailwind-and-pinia/src/views/Library.vue
@@ -1,15 +1,19 @@
 <template>
   <div
-    class="p-8 rounded-md grid 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6"
+    class="p-8 text-white rounded-md grid 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6"
   >
     <div v-for="(album, index) in albums" :key="index">
       <div
-        @click="selectAlbum(album)"
-        class="flex bg-gray-600 rounded-md duration-300 hover:cursor-pointer hover:bg-gray-300"
-      >
-        <img :src="album.album_cover" class="w-[60px] h-[60px] rounded-s-md" />
+        class="flex bg-gray-600 rounded-md duration-300 hover:cursor-pointer hover:bg-gray-700"
+      > 
+        <img
+          @click="selectAlbum(album)"
+          :src="album.album_cover"
+          class="w-[60px] h-[60px] rounded-s-md"
+        />
         <div class="ml-3 mr-5 flex items-center justify-between w-full">
           <p
+            @click="selectAlbum(album)"
             class="font-semibold w-[60px] xl:max-w-[160px] md:max-w-[100px] sm:w-[200px] whitespace-nowrap overflow-hidden text-ellipsis"
           >
             {{ album.album }}
@@ -23,6 +27,7 @@
   </div>
 </template>
 <script setup lang="ts">
+import { useRouter } from "vue-router";
 import { ref } from "vue";
 import { useSelectAlbumStore } from "../store/selectAlbum.ts";
 import all_albums from "../../artist.json";
@@ -34,8 +39,12 @@ import DotsHorizontal from "vue-material-design-icons/DotsHorizontal.vue";
 import Heart from "vue-material-design-icons/Heart.vue";
 import ClockTimeThreeOutline from "vue-material-design-icons/ClockTimeThreeOutline.vue";
 
+const router = useRouter();
 const albums = ref<IAlbum[]>(all_albums);
-const albumStore = useSelectAlbumStore()
+const albumStore = useSelectAlbumStore();
 //functions
-const selectAlbum = (selected_album:IAlbum) => albumStore.getSelectedAlbum(selected_album);
+const selectAlbum = (selected_album: IAlbum) => {
+  albumStore.getSelectedAlbum(selected_album);
+  router.push("/selected-album");
+};
 </script>
