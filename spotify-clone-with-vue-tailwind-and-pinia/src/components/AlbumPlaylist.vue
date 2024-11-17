d0a375ef629cb05e60ed832c6cc12c3663f28c81
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-11-17 01:58:32 -0400

    feat: putting play from the beginning to work

--- spotify-clone-with-vue-tailwind-and-pinia/src/components/AlbumPlaylist.vue
+++ spotify-clone-with-vue-tailwind-and-pinia/src/components/AlbumPlaylist.vue
@@ -3,28 +3,59 @@
     <div class="flex">
       <div>
         <div class="circle">
-          <Play :size="40" fillColor="#fff" />
+          <Play
+            @click="useSong.playFromTheBeginning()"
+            :size="40"
+            fillColor="#fff"
+            v-if="!is_playing"
+          />
+          <Pause
+            @click="useSong.playOrPauseThisSong(current_artist, current_track)"
+            :size="40"
+            fillColor="#fff"
+            v-else
+          />
         </div>
       </div>
-      <Heart class="hover:scale-125 duration-300 mt-1 ml-4" :size="40" fillColor="#fff" />
+      <Heart
+        class="hover:scale-125 duration-300 mt-1 ml-4"
+        :size="40"
+        fillColor="#fff"
+      />
     </div>
     <Songs />
   </div>
 </template>
 <script setup lang="ts">
+//MODULES AND INTERFACES
 import { ref } from "vue";
-import { useSelectAlbumStore } from "../store/selectAlbum.ts";
 import { IAlbum } from "../interfaces/albums";
 
-//components
+//COMPONENTS
 import Songs from "./Songs.vue";
 import Play from "vue-material-design-icons/Play.vue";
+import Pause from "vue-material-design-icons/Pause.vue";
 import Heart from "vue-material-design-icons/Heart.vue";
 
-//variables
-const albumStore = useSelectAlbumStore();
-const album_details = ref<IAlbum>(albumStore.album);
+//STUFF FROM PINIA
+import { storeToRefs } from "pinia";
+import { useSongStore } from "../store/song";
+const useSong = useSongStore();
+const { is_playing, current_track, current_artist, album } =
+  storeToRefs(useSong);
 
+//VARIABLES
+const album_details = ref<IAlbum>(album.value);
+
+//FUNCTIONS
+// const playFunc = () => {
+//   if (current_track.value) {
+//     useSong.playOrPauseThisSong(current_artist.value, current_track.value);
+//     return;
+//   }
+
+//   useSong.playFromTheBeginning();
+// };
 </script>
 <style scoped>
 .glass-effect {
