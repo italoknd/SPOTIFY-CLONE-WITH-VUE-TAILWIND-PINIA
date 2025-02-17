69cf10166c083c44f2972db9180b8d383f3b74e0
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2025-02-16 23:46:30 -0400

    import piniaPluginPersistt

--- spotify-clone-with-vue-tailwind-and-pinia/src/main.ts
+++ spotify-clone-with-vue-tailwind-and-pinia/src/main.ts
@@ -3,8 +3,13 @@ import { createPinia } from "pinia";
 import router from "./router";
 import App from "./App.vue";
 import "./style.css";
+import piniaPluginPersistedState from "pinia-plugin-persistedstate";
+
+const pinia = createPinia();
 
 const app = createApp(App);
-app.use(createPinia());
+
+pinia.use(piniaPluginPersistedState);
+app.use(pinia);
 app.use(router);
 app.mount("#app");
