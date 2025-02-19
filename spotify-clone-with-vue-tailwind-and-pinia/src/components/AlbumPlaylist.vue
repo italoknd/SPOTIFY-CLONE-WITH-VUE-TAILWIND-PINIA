cd359a4c0152f6d1a890a65f48d05f6531d024aa
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2025-02-19 11:36:19 -0400

    feat: save playlist on the store

--- spotify-clone-with-vue-tailwind-and-pinia/src/components/AlbumPlaylist.vue
+++ spotify-clone-with-vue-tailwind-and-pinia/src/components/AlbumPlaylist.vue
@@ -18,6 +18,7 @@
         </div>
       </div>
       <Heart
+        @click="addTracksToFavoritePlaylist()"
         class="hover:scale-125 duration-300 mt-1 ml-4"
         :size="40"
         fillColor="#fff"
@@ -36,7 +37,13 @@ import Heart from "vue-material-design-icons/Heart.vue";
 //STUFF FROM PINIA
 import { storeToRefs } from "pinia";
 import { useSongStore } from "../store/song";
+
 const useSong = useSongStore();
+
+const addTracksToFavoritePlaylist = (): void => {
+  useSong.addToLikedPlaylistTheEntireAlbum();
+};
+
 const { is_playing, current_track, current_artist } = storeToRefs(useSong);
 </script>
 <style scoped>
