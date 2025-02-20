4b85bdbebd87d3286b884db635cb819c19b42bfa
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2025-02-20 19:58:40 -0400

    feat: add some validations for album_details object on template

--- spotify-clone-with-vue-tailwind-and-pinia/src/components/AlbumCover.vue
+++ spotify-clone-with-vue-tailwind-and-pinia/src/components/AlbumCover.vue
@@ -2,45 +2,63 @@
   <div class="flex h-[400px]">
     <img
       class="w-[250px] h-[250px] rounded-md"
-      :src="album_details.album_cover"
+      :src="album_details?.album_cover"
       alt="Album cover art"
     />
     <div class="ml-3 w-full relative top-[120px]">
-      <p class="ml-2 text-md">{{ album_details.album_category }}</p>
+      <p class="ml-2 text-md">{{ album_details?.album_category }}</p>
 
       <h1
         class="font-extrabold text-6xl pb-5 whitespace-nowrap w-full text-ellipsis"
       >
-        {{ album_details.album }}
+        {{ album_details?.album }}
       </h1>
       <div class="flex items-center">
         <img
-          :src="album_details.artist_picture || album_details.playlist_owner_picture"
+          :src="
+            album_details?.artist_picture ||
+            album_details?.playlist_owner_picture
+          "
           alt="Artist Picture"
           class="w-[25px] h-[25px] rounded-xl object-contain"
         />
         <p class="mx-2 text-md">
-          <strong>{{ album_details.artist_of_the_album }}</strong>
+          <strong>{{ album_details?.artist_of_the_album }}</strong>
         </p>
-        <Dot />
-        <p class="mx-2 text-md">{{ album_details.release_year }}</p>
-        <Dot />
-        <p class="mx-2 text-md">
-          {{ album_details.tracks.length > 1 ? "songs" : "song" }},
-          {{ albumStore.playlist_duration }}
+        <Dot v-if="album_details?.release_year" />
+        <p class="mx-2 text-md">{{ album_details?.release_year }}</p>
+        <Dot v-if="album_details?.release_year" />
+        <p class="mx-2 text-md" v-if="album_details?.tracks?.length">
+          {{ (album_details?.tracks ?? []).length > 1 ? "songs" : "song," }}
+          {{ albumStore?.playlist_duration }}
         </p>
       </div>
     </div>
   </div>
 </template>
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import { useSongStore } from "../store/song";
 import { IAlbum } from "../interfaces/albums";
 
+import { useRoute } from "vue-router";
+
 //components
 import Dot from "../components/Dot.vue";
-
+const route = useRoute();
 const albumStore = useSongStore();
-const album_details = ref<IAlbum>(albumStore.album);
+const album_details = ref<IAlbum>();
+
+onMounted(() => {
+  validateSection();
+});
+
+//FUNCTIONS
+const validateSection = (): void => {
+  if (route.path.includes("liked")) {
+    album_details.value = albumStore.liked_songs;
+  } else if (route.path.includes("selected-album")) {
+    album_details.value = albumStore.album;
+  }
+};
 </script>
