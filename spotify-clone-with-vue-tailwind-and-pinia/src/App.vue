e12062b8718e1847a67ba30f4ec65e7ae33ce1bb
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2025-03-07 10:34:57 -0400

    create new components for header

--- spotify-clone-with-vue-tailwind-and-pinia/src/App.vue
+++ spotify-clone-with-vue-tailwind-and-pinia/src/App.vue
@@ -1,18 +1,14 @@
 <script setup lang="ts">
-//ICONS
-import ChevronUp from "vue-material-design-icons/ChevronUp.vue";
-import ChevronDown from "vue-material-design-icons/ChevronDown.vue";
-import ChevronRight from "vue-material-design-icons/ChevronRight.vue";
-import ChevronLeft from "vue-material-design-icons/ChevronLeft.vue";
-
 //COMPONENTS
 import MusicPlayer from "./components/MusicPlayer.vue";
 import MenuItem from "./components/MenuItem.vue";
+import HeaderDesktop from "./components/Header/HeaderDesktop.vue";
+import HeaderMobile from "./components/Header/HeaderMobile.vue"; 
 
 //MODULES
 import { useRouter } from "vue-router";
 import { storeToRefs } from "pinia";
-import { onMounted } from "vue";
+import { onMounted, onUnmounted } from "vue";
 
 //SCRIPTS
 import { side_items, side_items_p2 } from "./scripts/sideItems";
@@ -22,16 +18,9 @@ import { useSongStore } from "./store/song";
 const useSong = useSongStore();
 const { is_playing, current_track } = storeToRefs(useSong);
 
-//SPECIAL VARIABLES
-const router = useRouter();
-
 //VARIABLES
-let openMenu = $ref<boolean>(false);
-
-const username = $ref("Italo Pedroza");
-const userProfilePicture = $ref(
-  "https://lh3.googleusercontent.com/a/ACg8ocJ-zkH1FfQwQhEAJa5_07Pp2q_HVmFnXS_ce9xYtPHGbnk=s96-c-rg-br100"
-);
+const router = useRouter();
+let isMobile = $ref(window.innerWidth < 768);
 
 //HOOKS
 onMounted(() => {
@@ -39,76 +28,31 @@ onMounted(() => {
   setTimeout(() => useSong.verifyAndApplyNavigatorMediaSession(), 1000);
 });
 
-//FUNCTIONS
-const toggleOpeness = (param: boolean): void => {
-  openMenu = param;
-};
+onUnmounted(() => {
+  window.removeEventListener("resize", checkScreenSize);
+});
 
+//FUNCTIONS
 const routeTo = (toRoute: string): void => {
   router.push(toRoute);
+};
+
+const checkScreenSize = () => {
+  isMobile = window.innerWidth < 768;
 };
 </script>
 
 <template>
   <div>
     <n-notification-provider>
-      <div
-        class="w-[calc(100%-230px)] h-[60px] fixed right-0 z-20 bg-[#101010] bg-opacity-80 flex items-center justify-between"
-      >
-        <div class="flex items-center ml-6">
-          <button
-            @click="router.go(-1)"
-            type="button"
-            class="rounded-full bg-black p-[1px] cursor-pointer"
-          >
-            <ChevronLeft fillColor="#fff" :size="30" />
-          </button>
-          <button
-            @click="router.go(1)"
-            type="button"
-            class="rounded-full bg-black p-[1px] cursor-pointer ml-4"
-          >
-            <ChevronRight fillColor="#fff" :size="30" />
-          </button>
-        </div>
-
-        <!--PROFILE INFOS-->
-        <button
-          @click="toggleOpeness(!openMenu)"
-          :class="openMenu ? 'bg-secondary' : 'bg-black'"
-          class="bg-black hover:bg-secondary rounded-full p-0.5 mr-8 mt-0.5 cursor-pointer"
-        >
-          <div class="flex items-center">
-            <img
-              class="rounded-full"
-              width="27"
-              :src="userProfilePicture"
-              alt="Profile Picture"
-            />
-            <div class="text-white text-[14px] mx-1.5 font-semibold">
-              {{ username }}
-            </div>
-            <ChevronDown v-if="openMenu" fillColor="#fff" :size="25" />
-            <ChevronUp v-else fillColor="#fff" :size="25" />
-          </div>
-        </button>
-        <span
-          v-if="openMenu"
-          class="fixed w-[190px] bg-[#282828] shadow-2xl z-50 rounded-sm top-[52px] right-[35px] p-1 cursor-pointer"
-        >
-          <ul class="text-gray-200 font-semibold text-[14px]">
-            <li class="px-3 py-2.5 hover:bg-[#3e3d3d] border-b-gray-600">
-              Profile
-            </li>
-            <li class="px-3 py-2.5 hover:bg-[#3e3d3d] border-b-gray-600">
-              Log out
-            </li>
-          </ul>
-        </span>
-      </div>
+      <!--HEADER-->
+      <component :is="isMobile ? HeaderDesktop : HeaderMobile" />
 
       <!--SIDEBAR-->
-      <div class="h-[100%] p-6 w-[240px] fixed z-50 bg-black hidden lg:block" id="sideNav">
+      <div
+        class="h-[100%] p-6 w-[240px] fixed z-50 bg-black hidden lg:block"
+        id="sideNav"
+      >
         <div class="w-[200px] cursor-pointer">
           <div class="mb-4" @click="routeTo('/')">
             <img
---
+++ spotify-clone-with-vue-tailwind-and-pinia/src/components/Header/HeaderDesktop.vue
@@ -0,0 +1,77 @@
+<template>
+  <div
+    class="hidden w-[calc(100%-230px)] h-[60px] fixed right-0 z-20 bg-[#101010] bg-opacity-80 lg:flex items-center justify-between"
+  >
+    <div class="flex items-center ml-6">
+      <button
+        @click="router.go(-1)"
+        type="button"
+        class="rounded-full bg-black p-[1px] cursor-pointer"
+      >
+        <ChevronLeft fillColor="#fff" :size="30" />
+      </button>
+      <button
+        @click="router.go(1)"
+        type="button"
+        class="rounded-full bg-black p-[1px] cursor-pointer ml-4"
+      >
+        <ChevronRight fillColor="#fff" :size="30" />
+      </button>
+    </div>
+
+    <!--PROFILE INFOS-->
+    <button
+      @click="toggleOpeness(!openMenu)"
+      :class="openMenu ? 'bg-secondary' : 'bg-black'"
+      class="bg-black hover:bg-secondary rounded-full p-0.5 mr-8 mt-0.5 cursor-pointer"
+    >
+      <div class="flex items-center">
+        <img
+          class="rounded-full"
+          width="27"
+          :src="userStore.userProfilePicture"
+          alt="Profile Picture"
+        />
+        <div class="text-white text-[14px] mx-1.5 font-semibold">
+          {{ userStore.username }}
+        </div>
+        <ChevronDown v-if="openMenu" fillColor="#fff" :size="25" />
+        <ChevronUp v-else fillColor="#fff" :size="25" />
+      </div>
+    </button>
+    <span
+      v-if="openMenu"
+      class="fixed w-[190px] bg-[#282828] shadow-2xl z-50 rounded-sm top-[52px] right-[35px] p-1 cursor-pointer"
+    >
+      <ul class="text-gray-200 font-semibold text-[14px]">
+        <li class="px-3 py-2.5 hover:bg-[#3e3d3d] border-b-gray-600">
+          Profile
+        </li>
+        <li class="px-3 py-2.5 hover:bg-[#3e3d3d] border-b-gray-600">
+          Log out
+        </li>
+      </ul>
+    </span>
+  </div>
+</template>
+<script lang="ts">
+//ICONS
+import ChevronUp from "vue-material-design-icons/ChevronUp.vue";
+import ChevronDown from "vue-material-design-icons/ChevronDown.vue";
+import ChevronRight from "vue-material-design-icons/ChevronRight.vue";
+import ChevronLeft from "vue-material-design-icons/ChevronLeft.vue";
+
+//MODULES
+import { useUserStore } from "../../store/user";
+import { useRouter } from "vue-router";
+
+//VARIABLES
+const router = useRouter();
+const userStore = useUserStore();
+let openMenu = $ref<boolean>(false);
+
+//FUNCTIONS
+const toggleOpeness = (param: boolean): void => {
+  openMenu = param;
+};
+</script>
